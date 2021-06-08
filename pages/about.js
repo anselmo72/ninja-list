55adfef230467059c72f0fd42f388a2ae433229d
anselmo72 <anselmo.ribeiro@gmail.com>
2021-06-08 02:30:07 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+          recusandae, et iure aut cum quas ducimus sapiente accusantium
+          molestias labore, unde ipsa omnis vero atque harum soluta iste eaque
+          blanditiis?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea commodi
+          quis velit porro aliquid deserunt repellat ipsam aspernatur laborum
+          animi?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+          corporis!
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default About
